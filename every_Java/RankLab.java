4ec4ac8d00eac42e84c0a9947e1352e91c342602
Fujisan0209 <Fujisan0209@users.noreply.github.com>
2026-09-24 01:01:08 +0900

    RankLab途中まで

---
+++ every_Java/RankLab.java
@@ -0,0 +1,11 @@
+final class Player implements Comparable<Player> {
+    private final String name;
+    private final int score;
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    
+}
